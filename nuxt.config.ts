b7fb11637856ef376250f876fffb37f6a2eeab55
maxwell cofie <maxcofie@gmail.com>
2025-04-11 21:17:27 +0000

    added UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,24 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: {enabled: true},
-    modules: ['@nuxt/ui'],
+    modules: ['@nuxt/ui', '@nuxt/fonts'],
     css: ['~/assets/css/main.css'],
+    fonts: {
+        defaults: {
+            weights: [400],
+            styles: ['normal', 'italic'],
+            subsets: [
+                'cyrillic-ext',
+                'cyrillic',
+                'greek-ext',
+                'greek',
+                'vietnamese',
+                'latin-ext',
+                'latin',
+            ]
+        },
+    },
+    ui: {
+        colorMode: false
+    }
 })
